4defb8c9a0489a0c29bea159c60c0e1918758f68
jagathda <mail.jagath@gmail.com>
2024-08-25 23:29:00 +0300

    Added public subnet

--- vpc/main.tf
+++ vpc/main.tf
@@ -20,3 +20,14 @@ resource "aws_vpc" "main" {
     Environment = "dev"
   }
 }
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "test-public-subnet"
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
