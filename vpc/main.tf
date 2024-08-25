8272f8d6aae09213d5c54a8cf6169fd13373641e
jagathda <mail.jagath@gmail.com>
2024-08-25 23:37:19 +0300

    Added public route table

--- vpc/main.tf
+++ vpc/main.tf
@@ -2,7 +2,7 @@ resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = "test"
+    Name = "test-igw"
     Terraform = "true"
     Environment = "dev"
   }
@@ -15,7 +15,7 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = true
 
   tags = {
-    Name = "test"
+    Name = "test-vpc"
     Terraform = "true"
     Environment = "dev"
   }
@@ -27,6 +27,21 @@ resource "aws_subnet" "public_subnet" {
 
   tags = {
     Name = "test-public-subnet"
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "test-public-route-table"
     Terraform = "true"
     Environment = "dev"
   }
