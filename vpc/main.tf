cb67da98a2f26633ec453d13b010eb9d1ae063c6
jagathda <mail.jagath@gmail.com>
2024-08-26 00:28:28 +0300

    Added private and database routes

--- vpc/main.tf
+++ vpc/main.tf
@@ -117,3 +117,18 @@ resource "aws_nat_gateway" "gw" {
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public_subnet.id
 }
+
+//private route
+resource "aws_route" "private_route" {
+  route_table_id            = aws_route_table.private_route_table.id
+  destination_cidr_block    = "0.0.0.0./0"
+  nat_gateway_id =  aws_nat_gateway.gw.id
+}
+
+//database route
+resource "aws_route" "database_route" {
+  route_table_id            = aws_route_table.database_route_table.id
+  destination_cidr_block    = "0.0.0.0./0"
+  nat_gateway_id =  aws_nat_gateway.gw.id
+}
+
