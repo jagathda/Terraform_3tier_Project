5180c6625e40d0aa46213f3f4bf418f4cf5c27be
jagathda <mail.jagath@gmail.com>
2024-08-25 23:21:59 +0300

    Added internet gateway

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,3 +1,13 @@
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "test"
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
