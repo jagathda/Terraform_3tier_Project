134ce027c77c5e834666d234d2d32e9f2d849c84
jagathda <mail.jagath@gmail.com>
2024-08-28 16:54:03 +0300

    Added varibales to use in vpc

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -3,3 +3,32 @@ variable "project_name" {
   default = "test"
   description = "This is a test project"
 }
+
+variable "cidr_block" {
+  type = string
+  default = "10.0.0.0/16"
+}
+
+variable "instance_tenancy" {
+  type = string
+  default = "default"
+}
+
+variable "dns_support" {
+  type = bool
+  default = true
+}
+
+variable "dns_hostnames" {
+  type = bool
+  default = true
+}
+
+variable "tags" {
+  type = map(string)
+  default = {
+    Name        = "test"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
